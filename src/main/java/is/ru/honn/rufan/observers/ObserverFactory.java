127ee825a239593426619edd87b49a33db651dc1
Arni Benedikt Arnason <arnibarnason@gmail.com>
2015-09-22 14:41:35 +0000

    observer progress

---
+++ src/main/java/is/ru/honn/rufan/observers/ObserverFactory.java
@@ -0,0 +1,25 @@
+package is.ru.honn.rufan.observers;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import java.util.Observer;
+
+/**
+ * Created by arnib on 22/09/15.
+ */
+public class ObserverFactory
+{
+    public ObserverFactory()
+    {
+    }
+
+    public Observer getObserver(String observerType)
+    {
+        Observer observer;
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:observer.xml");
+        observer = (Observer) ctx.getBean(observerType);
+        System.out.println(observer.toString());
+        return observer;
+    }
+}
--- src/main/java/is/ru/honn/rufan/process/PlayerImportProcess.java
+++ src/main/java/is/ru/honn/rufan/process/PlayerImportProcess.java
@@ -1,6 +1,7 @@
 package is.ru.honn.rufan.process;
 
 import is.ru.honn.rufan.domain.Player;
+import is.ru.honn.rufan.observers.ObserverFactory;
 import is.ru.honn.rufan.observers.PlayerObserver;
 import is.ru.honn.rufan.reader.ReadHandler;
 import is.ru.honn.rufan.reader.Reader;
@@ -9,7 +10,9 @@ import is.ru.honn.rufan.reader.ReaderFactory;
 import is.ru.honn.rufan.service.PlayerServiceStub;
 import is.ru.honn.rufan.service.ServiceException;
 import is.ruframework.process.RuAbstractProcess;
+import org.springframework.context.MessageSource;
 
+import java.util.Observer;
 import java.util.logging.Logger;
 
 /**
@@ -20,15 +23,19 @@ public class PlayerImportProcess extends RuAbstractProcess implements ReadHandle
     Reader reader;
     Logger log = Logger.getLogger(PlayerImportProcess.class.getName());
     private PlayerServiceStub playerService = new PlayerServiceStub();
-    //MessageResource resource;
+    private ObserverFactory observerFactory = new ObserverFactory();
+    MessageSource msg;
 
     @Override
     public void beforeProcess()
     {
-        PlayerObserver observer = new PlayerObserver();
+        Observer observer = observerFactory.getObserver("playerObserver");
+        //Observer observer1 = observerFactory.getObserver("teamObserver");
+        // /PlayerObserver observer = new PlayerObserver();
         playerService.addObserver(observer);
         ReaderFactory factory = new ReaderFactory();
         reader = factory.getReader("PlayerReader");
+        msg = factory.getMessageSource("messageSource");
         reader.setReadHandler(this);
         log.info("start process");
     }
